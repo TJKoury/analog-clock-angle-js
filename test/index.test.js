a65bd3005e7f565b540bcb094f9beac4998a106e
TJKoury <TJ@bisonpc.net>
2014-09-17 08:42:17 -0400

    Added additional test cases

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ var getMinAngle = require('../index.js');
 describe('getMinAngle', function(done){
 
     
-    [[0,0,0],[3,0,90],[3,30,75],[6,0,180],[9,0,90],[11,59,5.5]].forEach(function(a){
+    [[0,0,0],[3,0,90],[3,30,75],[4,0,120],[6,0,180],[7,0,150],[9,0,90],[11,59,5.5]].forEach(function(a){
 
         it((a[0]+ ":" +a[1]+ " = " + getMinAngle(a[0],a[1])), function(done){
             console.log("Expecting: " + a[2]);
